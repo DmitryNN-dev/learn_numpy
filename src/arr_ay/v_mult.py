1f233ee64373157b6e3a8e495b1a66456ef9bec6
BlackCasque <tihonovdima3307@gmail.com>
2024-02-22 18:13:06 +0300

    outer

---
+++ src/arr_ay/v_mult.py
@@ -0,0 +1,10 @@
+import numpy as np
+x = np.arange(1, 6)
+
+# Массив повторяется в столбик, место пересечения дают остальные значения при переумножении:
+print(np.multiply.outer(x, x))
+
+print()
+
+# --//--, но add
+print(np.add.outer(x, x))
